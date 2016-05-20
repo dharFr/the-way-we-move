84329dcf16a3638e68aa021f8d4b76bb83f8e4bc
Olivier Audard <audard@gmail.com>
2016-05-21 00:34:32 +0200

    Add some analytics in production, so we know if someone's interested or not

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 const webpack = require('webpack')
 
+let entries = [
+  './src/polyfills/requestAnimationFrame.js',
+  './src/polyfills/getUserMedia.js',
+  './src/index.js'
+]
+
+if (process.env.NODE_ENV == 'production') {
+  entries.push('./src/stats/ga.js')
+}
+
 module.exports = {
-  entry: [
-    './src/polyfills/requestAnimationFrame.js',
-    './src/polyfills/getUserMedia.js',
-    './src/index.js'
-  ],
+  entry: entries,
   output: {
     filename: 'dist/bundle.js'
   },
   module: {
-
     loaders: [
       {
         test: /\.js$/,
