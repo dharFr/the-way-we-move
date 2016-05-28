f7a26bd90af4a265e3c5cae327ce8b90b2b57e40
Olivier Audard <audard@gmail.com>
2016-05-28 14:48:25 +0200

    work in progress...

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import controlPanel from './controlPanel.js'
 require('offline-plugin/runtime.js').install()
 
 const canvas         = document.getElementById('canvas')
+const video          = document.getElementById('video')
 const themeMeta      = document.head.querySelector('meta[name="theme-color"]')
 const backgroundMeta = document.head.querySelector('meta[name="background-color"]')
 const ctx            = canvas.getContext('2d')
@@ -15,14 +16,18 @@ const wfAnalyser     = new AudioAnalyser(1024)
 const fbgAnalyser    = new AudioAnalyser()
 
 let lasttime = 0
+let tracker = null
+let lastTrackerData = null
+
 function syncSize() {
-  canvas.width  = window.innerWidth
-  canvas.height = window.innerHeight
+  canvas.width  = video.width  = window.innerWidth
+  canvas.height = video.height = window.innerHeight
 }
 syncSize()
 
 window.addEventListener('resize', syncSize, false)
 canvas.addEventListener('click', _ => colors.nextPalette())
+
 
 function generateRandomPoints(numPoints, palette) {
   // Generate some random points
@@ -42,7 +47,31 @@ function addRandomPoint(colorIdx) {
   }))
 }
 
+function initTracker() {
+  // tracker is already initialized, skip this
+  if (tracker != null) {
+    return
+  }
+
+  // tracker is loaded asynchronously, may not be available on 1st calls
+  if (typeof tracking === 'undefined') {
+    return;
+  }
+
+  tracker = new tracking.ObjectTracker('face')
+  tracker.setInitialScale(4)
+  tracker.setStepSize(2)
+  tracker.setEdgesDensity(0.1)
+  tracking.track('#video', tracker, { camera: true })
+
+  tracker.on('track', function(event) {
+    lastTrackerData = event.data
+  })
+}
+
 function drawScene(timestamp=performance.now()) {
+
+  initTracker()
 
   if (lasttime == 0) {
     lasttime = timestamp
@@ -128,6 +157,18 @@ function drawScene(timestamp=performance.now()) {
 
   if (frequencyBarGraph) {
     fbgAnalyser.drawFrequencyBarGraph(drawParams)
+  }
+
+  if (true && lastTrackerData && lastTrackerData.length) {
+
+    lastTrackerData.forEach(function(rect) {
+      ctx.strokeStyle = palette.foreground()
+      ctx.strokeRect(rect.x, rect.y, rect.width, rect.height)
+      ctx.font = '11px Helvetica'
+      ctx.fillStyle = "#fff"
+      ctx.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11)
+      ctx.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22)
+    })
   }
 
   lasttime = timestamp
